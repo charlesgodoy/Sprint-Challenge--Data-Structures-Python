e03040f6d5eda4addafc1b9b64e5a2599d32a789
charlesgodoy <charlesgodoy@live.com>
2019-09-20 11:27:08 -0400

    working on ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,7 +5,14 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    if self.current == self.capacity:   # checks to see if full
+      self.current = 0
+      self.storage[self.current] = item
+      self.current += 1
+
+    else:
+      self.storage[self.current] = item   # if not full, then just add item and increase current
+      self.current += 1
 
   def get(self):
-    pass
+    return [i for i in self.storage if i]   # only returns true values
